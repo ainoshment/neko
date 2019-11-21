fd16a8b8a2b35ee32c7dd6f477e336ed0bfe7a73
ashino <m.ashino@9dw.jp>
2019-11-21 16:05:43 +0900

    [update] update docstring and value range

--- seconds2time.py
+++ seconds2time.py
@@ -2,15 +2,15 @@ from datetime import time
 
 
 def seconds2time(seconds):
-    """
-    :type seconds: int
-    :param seconds: 11111
-    :rtype: str,
-    :return: "03:05:11"
+    """Convert seconds to time
+    Args:
+        seconds (int): 11111
+    Returns
+        str: "03:05:11"
     """
     if type(seconds) is str:
         raise TypeError("an integer is required (got type str)")
-    if seconds > 86400 or seconds < 0:
-        raise ValueError("seconds must be int and in 0..86400")
+    if seconds >= 86400 or seconds < 0:
+        raise ValueError("seconds must be int and in 0..86399")
     hour, minute, second = seconds // 3600, (seconds % 3600) // 60, seconds % 60
     return str(time(hour=hour, minute=minute, second=second))
