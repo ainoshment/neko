2863c3c47f0c7cbcd7f7712496fd4c585e676974
ashino <m.ashino@9dw.jp>
2019-12-09 17:26:22 +0900

    [add] convert time to hours

---
+++ time2hours.py
@@ -0,0 +1,14 @@
+def time2hours(time):
+    """Convert time to hours
+    Args:
+        time (str): "03:05:21"
+    Returns
+        hours (int): 3.089166666666667
+    """
+    if type(time) is int:
+        raise TypeError("a string is required (got type int)")
+    hour = int(time.split(":")[0])
+    minute = int(time.split(":")[1])
+    second = int(time.split(":")[2])
+    hours = hour + (minute / 60) + (second / 3600)
+    return hours
