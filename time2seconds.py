2049f9dbc4c740299839ac83485a82814817023a
ashino <m.ashino@9dw.jp>
2019-12-09 16:00:07 +0900

    [add] fix

--- time2seconds.py
+++ time2seconds.py
@@ -6,7 +6,7 @@ def time2seconds(time):
         minutes (int):11111
     """
     if type(time) is int:
-        raise TypeError("an string is required (got type int)")
+        raise TypeError("a string is required (got type int)")
     unit = [3600, 60, 1]
     seconds = sum(int(x) * int(y) for (x, y) in zip(unit, time.split(":")))
     return seconds
