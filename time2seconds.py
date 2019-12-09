793f50be5aff8b1b24c23fc0f73ebe9a2287a28a
ashino <m.ashino@9dw.jp>
2019-12-09 15:59:44 +0900

    [add] convert time to seconds

---
+++ time2seconds.py
@@ -0,0 +1,12 @@
+def time2seconds(time):
+    """Convert time to seconds
+    Args:
+        time (str): "03:05:11"
+    Returns
+        minutes (int):11111
+    """
+    if type(time) is int:
+        raise TypeError("an string is required (got type int)")
+    unit = [3600, 60, 1]
+    seconds = sum(int(x) * int(y) for (x, y) in zip(unit, time.split(":")))
+    return seconds
